e1098b8b83348db39e106892fe12abec5e53c1d2
Dan Romans <danieljromans@gmail.com>
2019-07-24 01:04:46 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,4 @@
 def meal_choice(vegetable, starch, protien = "meat")
   puts "What a nutritious meal!"
-  puts "A plate of #{protien} with #{vegetable} and #{starch}."
   return "A plate of #{protien} with #{vegetable} and #{starch}."
 end
